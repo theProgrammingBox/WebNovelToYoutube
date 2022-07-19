557e094e4566108e3e7bb29d6351f760a9e79bbd
theProgrammingBox <justinboxgea@gmail.com>
2022-07-19 16:56:04 -0700

    ok

--- index.js
+++ index.js
@@ -1,83 +1,54 @@
 require("dotenv").config();
-// const puppeteer = require("puppeteer");
-// const gTTS = require("gtts");
-// var ffmpeg = require("fluent-ffmpeg");
+const puppeteer = require("puppeteer");
+const gTTS = require("gtts");
+var ffmpeg = require("fluent-ffmpeg");
+const getMP3Duration = require('get-mp3-duration')
+const fs = require('fs');
 
-// const numberOfProcesses = 10;
-// const maxChapters = 2; //process.env.MAX_CHAPTERS;
-// var chaptersDone = 0;
+const numberOfProcesses = 1; //10;
+const maxChapters = 1; //process.env.MAX_CHAPTERS;
+var chaptersDone = 0;
 
-// async function getChapter(chpt) {
-//   if (chpt > maxChapters) {
-//     return;
-//   }
-//   const browser = await puppeteer.launch();
-//   const page = await browser.newPage();
-//   await page.goto(`${process.env.NOVEL_URL}${chpt}`);
-//   const texts = await page.evaluate(() => {
-//     return Array.from(
-//       document.querySelectorAll(
-//         "body > div.wrap > div > div.site-content > div > div > div > div > div > div > div.c-blog-post > div.entry-content > div > div > div > div > p"
-//       )
-//     ).map((p) => p.innerText);
-//   });
-//   await browser.close();
-//   Promise.all(texts).then((texts) => {
-//     let gtts = new gTTS(texts.join(" "), "en");
-//     gtts.save(
-//       `./mp3/${process.env.NOVEL_NAME} chapter ${chpt}.mp3`,
-//       function (err, result) {
-//         if (err) {
-//           throw new Error(err);
-//         }
-//         let command = ffmpeg(process.env.IMAGE_PATH)
-//           .setFfmpegPath(process.env.FFMPEG_PATH)
-//           .input(`./mp3/${process.env.NOVEL_NAME} chapter ${chpt}.mp3`)
-//           .save(`./mp4/${process.env.NOVEL_NAME} chapter ${chpt}.mp4`)
-//           .on("end", function () {
-//             console.log(`${++chaptersDone} / ${maxChapters}`);
-//             getChapter(chpt + numberOfProcesses);
-//           })
-//           .on("error", function (err) {
-//             throw new Error(err);
-//           });
-//       }
-//     );
-//   });
-// }
-
-// console.log("Starting...");
-// for (let i = 1; i <= numberOfProcesses; i++) {
-//   getChapter(i);
-// }
-
-var videoshow = require('videoshow')
- 
-var images = [
-    process.env.IMAGE_PATH
-]
- 
-var videoOptions = {
-  fps: 25,
-  videoBitrate: 1024,
-  videoCodec: 'libx264',
-  size: '640x?',
-  audioBitrate: '128k',
-  audioChannels: 2,
-  format: 'mp4',
-  pixelFormat: 'yuv420p'
+async function getChapter(chpt) {
+  if (chpt > maxChapters) { return; }
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(`${process.env.NOVEL_URL}${chpt}`);
+  const texts = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll(
+      "body > div.wrap > div > div.site-content > div > div > div > div > div > div > div.c-blog-post > div.entry-content > div > div > div > div > p"
+    )).map((p) => p.innerText);
+  });
+  await browser.close();
+  Promise.all(texts).then((texts) => {
+    let gtts = new gTTS(texts.join(" "), "en");
+    let mp3Name = `./mp3/${process.env.NOVEL_NAME} chapter ${chpt}.mp3`;
+    let mp4Name = `./mp4/${process.env.NOVEL_NAME} chapter ${chpt}.mp4`;
+    gtts.save(
+      mp3Name,
+      function (err, result) {
+        if (err) { throw err; }
+        let command = ffmpeg(mp3Name)
+          .setFfmpegPath(process.env.FFMPEG_PATH)
+          .input(process.env.IMAGE_PATH)
+          .inputFPS(1)
+          .loop(getMP3Duration(fs.readFileSync(mp3Name)) * 0.001)
+          .save(mp4Name)
+          .on("end", function () {
+            fs.unlink(mp3Name, (err) => {
+              if (err) { throw err; }
+              console.log(`${mp3Name} deleted`);
+            });
+            console.log(`${++chaptersDone} / ${maxChapters}`);
+            getChapter(chpt + numberOfProcesses);
+          })
+          .on("error", function (err) { throw err; });
+      }
+    );
+  });
 }
- 
-videoshow(images, videoOptions)
-  .audio('./mp3/Overgeared chapter 1.mp3')
-  .save('./mp4/Overgeared chapter 1 Test.mp4')
-  .on('start', function (command) {
-    console.log('ffmpeg process started:', command)
-  })
-  .on('error', function (err, stdout, stderr) {
-    console.error('Error:', err)
-    console.error('ffmpeg stderr:', stderr)
-  })
-  .on('end', function (output) {
-    console.error('Video created in:', output)
-  })
+
+console.log("Starting...");
+for (let i = 1; i <= numberOfProcesses; i++) {
+  getChapter(i);
+}
